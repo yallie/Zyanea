e98bb6fce67caf1c7c0c986fbc20187fdafc4aee
yallie <yallie@yandex.ru>
2017-09-14 23:52:26 +0300

    Added the .editorconfig file.

--- src/src/MessageWire.Tests/RouterDealerTests.cs
+++ src/src/MessageWire.Tests/RouterDealerTests.cs
@@ -1,239 +1,237 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using MessageWire;
-using System.Text;
 using Xunit;
 using System.Threading;
 
 namespace MessageWire.Tests
 {
-    public class RouterDealerTests : IDisposable
-    {
-        public RouterDealerTests()
-        {
-            Wire.Linger = new TimeSpan(0, 0, 0, 1);
-        }
+	public class RouterDealerTests : IDisposable
+	{
+		public RouterDealerTests()
+		{
+			Wire.Linger = new TimeSpan(0, 0, 0, 1);
+		}
 
-        void IDisposable.Dispose()
-        {
-            Wire.Cleanup();
-        }
+		void IDisposable.Dispose()
+		{
+			Wire.Cleanup();
+		}
 
-        [Fact]
-        public void BasicSendReceiveTest()
-        {
-            var serverReceived = false;
-            var clientReceived = false;
-            var connString = "tcp://127.0.0.1:5800";
-            using (var server = new Host(connString))
-            {
-                server.MessageReceived += (s, e) =>
-                {
-                    Assert.Equal("Hello, I'm the client.", e.Message.Frames[0].ConvertToString());
-                    Assert.Equal("This is my second line.", e.Message.Frames[1].ConvertToString());
+		[Fact]
+		public void BasicSendReceiveTest()
+		{
+			var serverReceived = false;
+			var clientReceived = false;
+			var connString = "tcp://127.0.0.1:5800";
 
-                    var replyData = new List<byte[]>();
-                    replyData.Add("Hello, I'm the server. You sent.".ConvertToBytes());
-                    replyData.AddRange(e.Message.Frames);
-                    server.Send(e.Message.ClientId, replyData);
-                    serverReceived = true;
-                    Assert.True(e.Message.Frames.Count == 2, "Server received message did not have 2 frames.");
-                    Assert.True(replyData.Count == 3, "Server message did not have 3 frames.");
-                };
+			using (IHost server = new Host(connString))
+			{
+				server.MessageReceived += (s, e) =>
+				{
+					Assert.Equal("Hello, I'm the client.", e.Message.Frames[0].ConvertToString());
+					Assert.Equal("This is my second line.", e.Message.Frames[1].ConvertToString());
 
-                using (var client = new Client(connString))
-                {
-                    client.MessageReceived += (s, e) =>
-                    {
-                        clientReceived = true;
-                        Assert.True(e.Message.Frames.Count == 3, "Received message did not have 3 frames.");
-                        Assert.Equal("Hello, I'm the server. You sent.", e.Message.Frames[0].ConvertToString());
-                        Assert.Equal("Hello, I'm the client.", e.Message.Frames[1].ConvertToString());
-                        Assert.Equal("This is my second line.", e.Message.Frames[2].ConvertToString());
-                    };
+					var replyData = new List<byte[]>();
+					replyData.Add("Hello, I'm the server. You sent.".ConvertToBytes());
+					replyData.AddRange(e.Message.Frames);
+					server.Send(e.Message.ClientId, replyData);
+					serverReceived = true;
+					Assert.True(e.Message.Frames.Count == 2, "Server received message did not have 2 frames.");
+					Assert.True(replyData.Count == 3, "Server message did not have 3 frames.");
+				};
 
-                    var clientMessageData = new List<byte[]>();
-                    clientMessageData.Add("Hello, I'm the client.".ConvertToBytes());
-                    clientMessageData.Add("This is my second line.".ConvertToBytes());
-                    client.Send(clientMessageData);
+				using (IClient client = new Client(connString))
+				{
+					client.MessageReceived += (s, e) =>
+					{
+						clientReceived = true;
+						Assert.True(e.Message.Frames.Count == 3, "Received message did not have 3 frames.");
+						Assert.Equal("Hello, I'm the server. You sent.", e.Message.Frames[0].ConvertToString());
+						Assert.Equal("Hello, I'm the client.", e.Message.Frames[1].ConvertToString());
+						Assert.Equal("This is my second line.", e.Message.Frames[2].ConvertToString());
+					};
 
-                    var count = 0;
-                    while (count < 20 && (!clientReceived || !serverReceived))
-                    {
-                        Thread.Sleep(20);
-                        count++;
-                    }
-                    Assert.True(count < 100, "Test took too long.");
-                    Assert.True(serverReceived, "Server failed to receive and send.");
-                    Assert.True(clientReceived, "Client failed to receive.");
-                }
-            }
-        }
+					var clientMessageData = new List<byte[]>();
+					clientMessageData.Add("Hello, I'm the client.".ConvertToBytes());
+					clientMessageData.Add("This is my second line.".ConvertToBytes());
+					client.Send(clientMessageData);
 
-        [Fact]
-        public void EncryptedSendReceiveTest()
-        {
-            var serverReceived = false;
-            var clientReceived = false;
-            var connString = "tcp://127.0.0.1:5800";
-            using (var server = new Host(connString, new TestZkRepository()))
-            {
-                server.MessageReceived += (s, e) =>
-                {
-                    Assert.Equal("Hello, I'm the client.", e.Message.Frames[0].ConvertToString());
-                    Assert.Equal("This is my second line.", e.Message.Frames[1].ConvertToString());
+					var count = 0;
+					while (count < 20 && (!clientReceived || !serverReceived))
+					{
+						Thread.Sleep(20);
+						count++;
+					}
+					Assert.True(count < 100, "Test took too long.");
+					Assert.True(serverReceived, "Server failed to receive and send.");
+					Assert.True(clientReceived, "Client failed to receive.");
+				}
+			}
+		}
 
-                    var replyData = new List<byte[]>();
-                    replyData.Add("Hello, I'm the server. You sent.".ConvertToBytes());
-                    replyData.AddRange(e.Message.Frames);
-                    server.Send(e.Message.ClientId, replyData);
-                    serverReceived = true;
-                    Assert.True(e.Message.Frames.Count == 2, "Server received message did not have 2 frames.");
-                    Assert.True(replyData.Count == 3, "Server message did not have 3 frames.");
-                };
+		[Fact]
+		public void EncryptedSendReceiveTest()
+		{
+			var serverReceived = false;
+			var clientReceived = false;
+			var connString = "tcp://127.0.0.1:5800";
 
-                using (var client = new Client(connString, "testid", "....++++...."))
-                {
-                    var established = false;
-                    client.EcryptionProtocolEstablished += (s, e) =>
-                    {
-                        established = true;
-                    };
+			using (IHost server = new Host(connString, new TestZkRepository()))
+			{
+				server.MessageReceived += (s, e) =>
+				{
+					Assert.Equal("Hello, I'm the client.", e.Message.Frames[0].ConvertToString());
+					Assert.Equal("This is my second line.", e.Message.Frames[1].ConvertToString());
 
-                    client.EcryptionProtocolFailed += (s, e) =>
-                    {
-                        Assert.True(false, "Protocol failed.");
-                    };
+					var replyData = new List<byte[]>();
+					replyData.Add("Hello, I'm the server. You sent.".ConvertToBytes());
+					replyData.AddRange(e.Message.Frames);
+					server.Send(e.Message.ClientId, replyData);
+					serverReceived = true;
+					Assert.True(e.Message.Frames.Count == 2, "Server received message did not have 2 frames.");
+					Assert.True(replyData.Count == 3, "Server message did not have 3 frames.");
+				};
 
-                    client.MessageReceived += (s, e) =>
-                    {
-                        clientReceived = true;
-                        Assert.True(e.Message.Frames.Count == 3, "Received message did not have 3 frames.");
-                        Assert.Equal("Hello, I'm the server. You sent.", e.Message.Frames[0].ConvertToString());
-                        Assert.Equal("Hello, I'm the client.", e.Message.Frames[1].ConvertToString());
-                        Assert.Equal("This is my second line.", e.Message.Frames[2].ConvertToString());
-                    };
+				using (IClient client = new Client(connString, "testid", "....++++...."))
+				{
+					var established = false;
+					client.EcryptionProtocolEstablished += (s, e) =>
+					{
+						established = true;
+					};
 
-                    client.SecureConnection(blockUntilComplete: false);
+					client.EcryptionProtocolFailed += (s, e) =>
+					{
+						Assert.True(false, "Protocol failed.");
+					};
 
-                    var count = 0;
-                    while (count < 20000 && !established)
-                    {
-                        Thread.Sleep(20);
-                        count++;
-                    }
-                    Assert.True(count < 20000, "SecureConnection took too long.");
-                    Assert.True(established, "SecureConnection not established.");
+					client.MessageReceived += (s, e) =>
+					{
+						clientReceived = true;
+						Assert.True(e.Message.Frames.Count == 3, "Received message did not have 3 frames.");
+						Assert.Equal("Hello, I'm the server. You sent.", e.Message.Frames[0].ConvertToString());
+						Assert.Equal("Hello, I'm the client.", e.Message.Frames[1].ConvertToString());
+						Assert.Equal("This is my second line.", e.Message.Frames[2].ConvertToString());
+					};
 
-                    var clientMessageData = new List<byte[]>();
-                    clientMessageData.Add("Hello, I'm the client.".ConvertToBytes());
-                    clientMessageData.Add("This is my second line.".ConvertToBytes());
-                    client.Send(clientMessageData);
+					client.SecureConnection(blockUntilComplete: false);
 
-                    count = 0;
-                    while (count < 20 && (!established || !clientReceived || !serverReceived))
-                    {
-                        Thread.Sleep(20);
-                        count++;
-                    }
-                    Assert.True(count < 20, "Test took too long.");
-                    Assert.True(serverReceived, "Server failed to receive and send.");
-                    Assert.True(clientReceived, "Client failed to receive.");
-                }
-            }
-        }
+					var count = 0;
+					while (count < 20000 && !established)
+					{
+						Thread.Sleep(20);
+						count++;
+					}
+					Assert.True(count < 20000, "SecureConnection took too long.");
+					Assert.True(established, "SecureConnection not established.");
 
+					var clientMessageData = new List<byte[]>();
+					clientMessageData.Add("Hello, I'm the client.".ConvertToBytes());
+					clientMessageData.Add("This is my second line.".ConvertToBytes());
+					client.Send(clientMessageData);
 
-        [Fact]
-        public void HeartBeatTest()
-        {
-            var serverReceived = false;
-            var clientReceived = false;
-            var connString = "tcp://127.0.0.1:5800";
-            using (var server = new Host(connString, new TestZkRepository()))
-            {
-                server.MessageReceived += (s, e) =>
-                {
-                    Assert.Equal("Hello, I'm the client.", e.Message.Frames[0].ConvertToString());
-                    Assert.Equal("This is my second line.", e.Message.Frames[1].ConvertToString());
+					count = 0;
+					while (count < 20 && (!established || !clientReceived || !serverReceived))
+					{
+						Thread.Sleep(20);
+						count++;
+					}
+					Assert.True(count < 20, "Test took too long.");
+					Assert.True(serverReceived, "Server failed to receive and send.");
+					Assert.True(clientReceived, "Client failed to receive.");
+				}
+			}
+		}
 
-                    var replyData = new List<byte[]>();
-                    replyData.Add("Hello, I'm the server. You sent.".ConvertToBytes());
-                    replyData.AddRange(e.Message.Frames);
-                    server.Send(e.Message.ClientId, replyData);
-                    serverReceived = true;
-                    Assert.True(e.Message.Frames.Count == 2, "Server received message did not have 2 frames.");
-                    Assert.True(replyData.Count == 3, "Server message did not have 3 frames.");
-                };
+		[Fact]
+		public void HeartBeatTest()
+		{
+			var serverReceived = false;
+			var clientReceived = false;
+			var connString = "tcp://127.0.0.1:5800";
 
-                using (var client = new Client(connString, "testid", "....++++....", 
-                    heartBeatIntervalMs: 1000, maxSkippedHeartBeatReplies: 4))
-                {
-                    var established = false;
-                    client.EcryptionProtocolEstablished += (s, e) =>
-                    {
-                        established = true;
-                    };
+			using (IHost server = new Host(connString, new TestZkRepository()))
+			{
+				server.MessageReceived += (s, e) =>
+				{
+					Assert.Equal("Hello, I'm the client.", e.Message.Frames[0].ConvertToString());
+					Assert.Equal("This is my second line.", e.Message.Frames[1].ConvertToString());
 
-                    client.EcryptionProtocolFailed += (s, e) =>
-                    {
-                        Assert.True(false, "Protocol failed.");
-                    };
+					var replyData = new List<byte[]>();
+					replyData.Add("Hello, I'm the server. You sent.".ConvertToBytes());
+					replyData.AddRange(e.Message.Frames);
+					server.Send(e.Message.ClientId, replyData);
+					serverReceived = true;
+					Assert.True(e.Message.Frames.Count == 2, "Server received message did not have 2 frames.");
+					Assert.True(replyData.Count == 3, "Server message did not have 3 frames.");
+				};
 
-                    client.MessageReceived += (s, e) =>
-                    {
-                        clientReceived = true;
-                        Assert.True(e.Message.Frames.Count == 3, "Received message did not have 3 frames.");
-                        Assert.Equal("Hello, I'm the server. You sent.", e.Message.Frames[0].ConvertToString());
-                        Assert.Equal("Hello, I'm the client.", e.Message.Frames[1].ConvertToString());
-                        Assert.Equal("This is my second line.", e.Message.Frames[2].ConvertToString());
-                    };
+				using (IClient client = new Client(connString, "testid", "....++++....",
+					heartBeatIntervalMs: 1000, maxSkippedHeartBeatReplies: 4))
+				{
+					var established = false;
+					client.EcryptionProtocolEstablished += (s, e) =>
+					{
+						established = true;
+					};
 
-                    client.SecureConnection(blockUntilComplete: false);
+					client.EcryptionProtocolFailed += (s, e) =>
+					{
+						Assert.True(false, "Protocol failed.");
+					};
 
-                    var count = 0;
-                    while (count < 200 && !established)
-                    {
-                        Thread.Sleep(20);
-                        count++;
-                    }
-                    Assert.True(count < 200, "SecureConnection took too long.");
-                    Assert.True(established, "SecureConnection not established.");
+					client.MessageReceived += (s, e) =>
+					{
+						clientReceived = true;
+						Assert.True(e.Message.Frames.Count == 3, "Received message did not have 3 frames.");
+						Assert.Equal("Hello, I'm the server. You sent.", e.Message.Frames[0].ConvertToString());
+						Assert.Equal("Hello, I'm the client.", e.Message.Frames[1].ConvertToString());
+						Assert.Equal("This is my second line.", e.Message.Frames[2].ConvertToString());
+					};
 
-                    var clientMessageData = new List<byte[]>();
-                    clientMessageData.Add("Hello, I'm the client.".ConvertToBytes());
-                    clientMessageData.Add("This is my second line.".ConvertToBytes());
-                    client.Send(clientMessageData);
+					client.SecureConnection(blockUntilComplete: false);
 
-                    count = 0;
-                    while (count < 20 && (!established || !clientReceived || !serverReceived))
-                    {
-                        Thread.Sleep(20);
-                        count++;
-                    }
-                    Assert.True(count < 20, "Test took too long.");
-                    Assert.True(serverReceived, "Server failed to receive and send.");
-                    Assert.True(clientReceived, "Client failed to receive.");
+					var count = 0;
+					while (count < 200 && !established)
+					{
+						Thread.Sleep(20);
+						count++;
+					}
+					Assert.True(count < 200, "SecureConnection took too long.");
+					Assert.True(established, "SecureConnection not established.");
 
-                    count = 0;
-                    while (count < 200 && client.HeartBeatsSentCount < 2 && client.HeartBeatsReceivedCount < 2)
-                    {
-                        Thread.Sleep(500);
-                        count++;
-                    }
+					var clientMessageData = new List<byte[]>();
+					clientMessageData.Add("Hello, I'm the client.".ConvertToBytes());
+					clientMessageData.Add("This is my second line.".ConvertToBytes());
+					client.Send(clientMessageData);
 
-                    Assert.True(count < 200, "Test took too long.");
-                    var serverKeys = server.GetCurrentSessionKeys();
-                    Assert.Equal(1, serverKeys.Length);
-                    var session = server.GetSession(serverKeys[0]);
-                    Assert.NotNull(session);
-                    Assert.True("testid" == session.ClientIdentity, "Client identity does not match.");
-                    Assert.True(client.ClientId == session.ClientId, "ClientId does not match.");
-                    Assert.True(session.HeartBeatsReceivedCount > 1, "Server failed to receive heartbeats.");
-                }
-            }
-        }
-    }
+					count = 0;
+					while (count < 20 && (!established || !clientReceived || !serverReceived))
+					{
+						Thread.Sleep(20);
+						count++;
+					}
+					Assert.True(count < 20, "Test took too long.");
+					Assert.True(serverReceived, "Server failed to receive and send.");
+					Assert.True(clientReceived, "Client failed to receive.");
+
+					count = 0;
+					while (count < 200 && client.HeartBeatsSentCount < 2 && client.HeartBeatsReceivedCount < 2)
+					{
+						Thread.Sleep(500);
+						count++;
+					}
+
+					Assert.True(count < 200, "Test took too long.");
+					var serverKeys = server.GetCurrentSessionKeys();
+					Assert.Equal(1, serverKeys.Length);
+					var session = server.GetSession(serverKeys[0]);
+					Assert.NotNull(session);
+					Assert.True("testid" == session.ClientIdentity, "Client identity does not match.");
+					Assert.True(client.ClientId == session.ClientId, "ClientId does not match.");
+					Assert.True(session.HeartBeatsReceivedCount > 1, "Server failed to receive heartbeats.");
+				}
+			}
+		}
+	}
 }
